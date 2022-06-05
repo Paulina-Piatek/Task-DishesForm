8df9705a27239bd5f71dd07ac09de09bac21cf1c
Paulina <fridaycmg@gmail.com>
2022-06-05 21:38:20 +0200

    Change

--- src/components/Dishes.js
+++ src/components/Dishes.js
@@ -1,33 +1,37 @@
-import React, {useState} from "react"
-import { useForm } from "react-hook-form"
-import "../styles/dishes.css"
-import "../index.css"
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import "../styles/dishes.css";
+import "../index.css";
 
 const Dishes = () => {
-    const [selectedDishType,setselectedDishType] = useState("pizza")
-    const {
-        register,
-        handleSubmit,
-        formState: { errors } 
-    }= useForm();
-      // const onSubmit = data => alert(JSON.stringify(data));
-       const onSubmit = data => { fetch("https://frosty-wood-6558.getsandbox.com:443/dishes",
-         {method:"POST", body:JSON.stringify(data)});
-         alert("Success! You send Form !"); }
-    const Dish_types = [
-        {
-          id: "pizza",
-          label: "Pizza"
-        },
-        {
-          id: "soup",
-          label: "Soup"
-        },
-        {
-          id: "sandwich",
-          label: "Sandwich"
-        }
-      ];
+  const [selectedDishType, setselectedDishType] = useState("pizza");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // const onSubmit = data => alert(JSON.stringify(data));
+  const onSubmit = (data) => {
+    fetch("https://frosty-wood-6558.getsandbox.com:443/dishes", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    alert("Success! You send Form !");
+  };
+  const Dish_types = [
+    {
+      id: "pizza",
+      label: "Pizza",
+    },
+    {
+      id: "soup",
+      label: "Soup",
+    },
+    {
+      id: "sandwich",
+      label: "Sandwich",
+    },
+  ];
   console.log("errors", errors);
   const PizzaOptions = () => {
     if (selectedDishType !== "pizza") return null;
@@ -35,20 +39,22 @@ const Dishes = () => {
     return (
       <>
         <div className="form-group">
-          <input className="form-input"
+          <input
+            className="form-input"
             type="number"
             step="1"
-            {...register("no_of_slices", { required: true})}
+            {...register("no_of_slices", { required: true })}
             defaultValue="1"
           />
           <label className="form-label"># of slices </label>
         </div>
         <div className="form-group">
-          <input className="form-input"
+          <input
+            className="form-input"
             type="number"
             step="0.01"
             {...register("diameter", { required: true })}
-            defaultValue = "6"
+            defaultValue="6"
           />
           <label className="form-label">Diameter </label>
         </div>
@@ -62,12 +68,17 @@ const Dishes = () => {
     return (
       <>
         <div className="form-group">
-          <input className="form-input"
+          <input
+            className="form-input"
             type="range"
             min="1"
             max="10"
             step="1"
-            {...register("spiciness_scale", { required: true, min: 1, max: 10 })}
+            {...register("spiciness_scale", {
+              required: true,
+              min: 1,
+              max: 10,
+            })}
             defaultValue="5"
           />
           <label className="form-label">Spiciness Scale</label>
@@ -82,10 +93,11 @@ const Dishes = () => {
     return (
       <>
         <div className="form-group">
-          <input className="form-input"
+          <input
+            className="form-input"
             type="number"
             step="1"
-            {...register("slices_of_bread", { required: true})}
+            {...register("slices_of_bread", { required: true })}
             defaultValue="2"
           />
           <label className="form-label"># of slices</label>
@@ -94,45 +106,64 @@ const Dishes = () => {
     );
   };
   return (
-    <div className="dish"> 
-    <form onSubmit={handleSubmit(onSubmit)}>
-    <div className="dish-form">
-    <h2 className="form-title">Dishes Form</h2>
-    <p className='error'>{errors.name?.type === 'required' && "Dish Name is required"}</p>
-    <div className="form-group">
-        <input className="form-input" type="text"  {...register("name", { required: true })}  />
-        <label className="form-label">Dish name</label>
+    <div className="dish">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="dish-form">
+          <h2 className="form-title">Dishes Form</h2>
+          <p className="error">
+            {errors.name?.type === "required" && "Dish Name is required"}
+          </p>
+          <div className="form-group">
+            <input
+              className="form-input"
+              type="text"
+              {...register("name", { required: true })}
+            />
+            <label className="form-label">Dish name</label>
+          </div>
+          <p className="error">
+            {errors.preparation_time?.type === "required" && "Time is required"}
+          </p>
+          <div className="form-group">
+            <input
+              className="form-input-2"
+              type="time"
+              step="1"
+              {...register("preparation_time", { required: true })}
+              defaultValue="00:00:00"
+            />
+            <label className="form-label">Time</label>
+          </div>
+          <div className="form-group">
+            <select
+              className="form-input-2"
+              value={selectedDishType}
+              {...register("type", { required: true })}
+              onChange={(e) => {
+                const selectedType = e.target.value;
+                setselectedDishType(selectedType);
+              }}
+            >
+              {Dish_types.map(({ id, label }) => (
+                <option key={id} value={id}>
+                  {label}
+                </option>
+              ))}
+            </select>
+            <label className="form-label">Dish Type</label>
+          </div>
+          <PizzaOptions />
+          <SoupOptions />
+          <SandwichOptions />
+          <div className="input-field">
+            <button className="form-btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-    <p className='error'>{errors.preparation_time?.type === 'required' && "Time is required"}</p>
-    <div className="form-group">
-    <input className="form-input-2" type="time"  step="1"  {...register("preparation_time", { required: true })} defaultValue="00:00:00"/>
-        <label className="form-label">Time</label>
-    </div>
-    <div className="form-group">
-    <select className="form-input-2" value={selectedDishType}
-          {...register("type", { required: true })}
-          onChange={(e)=>{
-            const selectedType=e.target.value;
-            setselectedDishType(selectedType);
-        } }>
-          {Dish_types.map(({ id, label }) => (
-            <option key={id} value={id}>
-            {label}
-            </option>
-          ))}
-        </select>
-        <label className="form-label">Dish Type</label>
-    </div>
-      <PizzaOptions />
-      <SoupOptions />
-      <SandwichOptions />
-      <div className="input-field">
-        <button className="form-btn" type="submit" >Submit</button>
-      </div>
-    </div> 
-    </form>
-    </div> 
-  )
+  );
 };
 
 export default Dishes;
